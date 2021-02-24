550eb1bcde38f6aa136e7f33bd6038fda7d63f7b
legendarykeys <79459737+legendarykeys@users.noreply.github.com>
2021-02-24 21:53:26 +0545

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -52,8 +52,8 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
+I'm a powerful group management bot, 
+Add me to your groups as admin to manage your groups with my commands.
 You can find my list of available commands with /help.
 """
 
@@ -64,8 +64,7 @@ the things I can help you with.
 
 *Main* commands available:
  • /help: PM's you this message.
- • /help <module name>: PM's you info about that module.
- • /donate: information on how to donate!
+ • /donate: Information, How to donate!
  • /settings:
    • in PM: will send you your settings for all supported modules.
    • in a group: will redirect you to pm, with all that chat's settings.

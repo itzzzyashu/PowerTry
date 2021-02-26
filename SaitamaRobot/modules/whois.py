a378f864fe0d90c3fcb7c8e0f0133b2c6d386433
legendarykeys <79459737+legendarykeys@users.noreply.github.com>
2021-02-26 20:04:08 +0545

    Update whois.py

--- SaitamaRobot/modules/whois.py
+++ SaitamaRobot/modules/whois.py
@@ -1,19 +1,4 @@
 from datetime import datetime
-import html
-import json
-import os
-import psutil
-import random
-import time
-import datetime
-from typing import Optional, List
-import re
-import requests
-from telegram.error import BadRequest
-from telegram import Message, Chat, Update, Bot, MessageEntity
-from telegram import ParseMode
-from telegram.ext import CommandHandler, run_async, Filters
-from telegram.utils.helpers import escape_markdown, mention_html
 
 from pyrogram import filters
 from pyrogram.types import User, Message
@@ -33,11 +18,11 @@ def ReplyCheck(message: Message):
     return reply_id
 
 infotext = (
-    "**•[{full_name}](tg://user?id={user_id})**\n"
+    "**[{full_name}](tg://user?id={user_id})**\n"
     " * UserID: `{user_id}`\n"
     " * First Name: `{first_name}`\n"
     " * Last Name: `{last_name}`\n"
-    " * Username: `@{username}`\n"
+    " * Username: `{username}`\n"
     " * Last Online: `{last_online}`\n"
     " * Bio: {bio}")
 
@@ -78,22 +63,7 @@ async def whois(client, message):
         user = await client.get_users(get_user)
     except PeerIdInvalid:
         await message.reply("I don't know that User.")
-        mod_info = mod.__user_info__(user.id)
-        except TypeError:
-            mod_info = mod.__user_info__(user.id, chat.id)
-        if mod_info:
-            text += "\n" + mod_info 
         return
-        mod_info = mod.__user_info__(user.id, chat.id)
-        if mod_info:
-            text += "\n" + mod_info
-    try:
-        profile = bot.get_user_profile_photos(user.id).photos[0][-1]
-        bot.sendChatAction(chat.id, "upload_photo")
-        bot.send_photo(chat.id, photo=profile, caption=(text), parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-    except IndexError:
-        update.effective_message.reply_text(text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-
     desc = await client.get_chat(get_user)
     desc = desc.description
     await message.reply_text(
